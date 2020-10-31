86562526cdefbf0a6d7842fe7eda92d2ea716502
Feriel Nabet <fnabet@ipconnex.com>
2020-10-31 12:21:07 -0400

    exercise-1 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Header from './Header';
 import ChatStream from './ChatStream';
 import Footer from './Footer';
@@ -7,13 +6,14 @@ import Footer from './Footer';
 import './App.css';
 
 const App = (props) => {
-  return (
-    <div className='wrapper'>
-      <Header />
-      <ChatStream />
-      <Footer />
-    </div>
-  );
+    console.log(props)
+    
+    return ( <div className = 'wrapper' >
+        <Header / >
+        <ChatStream  messages={props.conversation.messages}/ >
+        <Footer / >
+        </div>
+    );
 };
 
 export default App;
