c773b44ef531020188e3e52e075805e4702a4582
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-11-01 14:22:50 -0500

    Two messages side by side

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import data from './data';
+import data2 from './data2';
 import App from './components/App';
 
 import './styles.css';
@@ -9,10 +10,17 @@ import './styles.css';
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <App 
+  <div style={{display: "flex"}}>
+    <App 
     currentUser={data.currentUser} 
     conversation={data.conversation} 
     participants={data.conversation.participants}
-  />,
+    />
+    <App 
+    currentUser={data2.currentUser} 
+    conversation={data2.conversation} 
+    participants={data2.conversation.participants}
+    />
+  </div>,
   rootElement
 );
