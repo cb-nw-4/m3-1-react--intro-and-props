b8d71f8a8d8c73d0a0bc494f8a80b446ae68bd32
Doug Wright <dw@nof49.ca>
2020-11-02 14:54:19 -0500

    Exercise 4 complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Header from './Header';
 import ChatStream from './ChatStream';
 import Footer from './Footer';
@@ -10,7 +9,9 @@ const App = (props) => {
   //console.log(props);
   return (
     <div className='wrapper'>
-      <Header />
+      <Header
+        participants={props.conversation.participants}
+        currentUser={props.currentUser} />
       <ChatStream
         messages={props.conversation.messages}
         currentUser={props.currentUser} />
