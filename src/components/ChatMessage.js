d273b7909a538c14098f0de3a2c5de236f360eca
Mallory Belgica <mallorybelgica@gmail.com>
2020-11-02 20:41:46 -0500

    Answered #5

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -6,7 +6,9 @@ const ChatMessage = (props) => {
     return <div className={`chat-message ${props.messageSent ? "sent" : "rcvd"}`}>
         <div className="user-name">{props.message.user.username}</div>
         <img className="user-avatar" src={props.message.user.avatar} alt="" />
+        <img className="tip-rcvd" src="/assets/tip-received.svg" />
         <div className="user-message">{props.message.body}</div>
+        <img className="tip-sent" src="/assets/tip-sent.svg" />
     </div>;
 };
 
