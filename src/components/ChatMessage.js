fcee3de617ed4efa0e61f725858b620c48870dda
Alexandre <alex_18h@hotmail.com>
2020-11-03 21:38:42 -0500

    exercise 3

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -3,13 +3,34 @@ import './ChatMessage.css';
 
 
 const ChatMessage = (props) => {
-    return <div className='chat-message'>
-        <img className="user-avatar" src={props.message.user.avatar}></img>
-    <div> 
-        <p className="user-name">{props.message.user.username}</p>
-        <p className="user-message">{props.message.body}</p>
-        </div>
-    </div>;
-};
 
+    if (props.messageType === 'sent') {
+        return <SentMessage message={props.message} />;
+    } 
+    else {
+        return <ReceivedMessage message={props.message} />;
+    }
+    };
+
+
+    const SentMessage = (props) => {
+        return ( 
+            <div className='chat-message'>
+                    <p className="user-message message-sent">{props.message.body}</p>
+            </div>
+                );
+    }
+
+
+    const ReceivedMessage = (props) => {
+        return ( 
+            <div className='chat-message'>
+                <img className="user-avatar" src={props.message.user.avatar} alt=""></img>
+                <div> 
+                    <p className="user-name">{props.message.user.username}</p>
+                    <p className="user-message">{props.message.body}</p>
+                </div>
+            </div>
+                );
+    }
 export default ChatMessage;
--- src/components/ChatStream.js
+++ src/components/ChatStream.js
@@ -14,7 +14,19 @@ const ChatStream = (props) => {
   console.log(props);
   return <section className='chat-stream'>
     {props.messages.map((message) => {
-    return (<ChatMessage key = {message.id} message = {message}/>);
+      let messageType = "sent";
+      if (message.user === props.currentUser){
+        messageType = "sent"
+      }
+      else {
+        messageType = "received"
+      }
+    return (<ChatMessage 
+      key = {message.id} 
+      message = {message}
+      user = {message.user}
+      messageType = {messageType}
+      />);
 })}
     </section>;
 
